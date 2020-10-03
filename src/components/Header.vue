72be5f14e739411b3ea768f025f62070ecb28044
Lougos <43130140+Lougos@users.noreply.github.com>
2020-10-03 17:31:51 +0200

    Install bootstrap

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,20 @@
 <template>
-
-    <a>
+    <nav>
+        <a class="btn btn-primary">My project</a>
+        <a>Compétence</a>
+        <a>Contact</a>
+    </nav>
 
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+    nav{
+        background-color: red;
+    }
+</style>
