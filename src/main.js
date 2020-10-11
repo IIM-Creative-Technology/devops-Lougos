92022371791f8cb4002bf33e55c12a195cd677b6
Lougos <43130140+Lougos@users.noreply.github.com>
2020-10-11 20:25:28 +0200

    Ouais les liens

--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Homepage from './components/Homepage.vue'
+import Priska from './components/Priska.vue'
+import Propos from './components/Propos.vue'
+import Projet from './components/Projet.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    mode:'history',
+    routes: [
+      {
+        path:'/',
+        name: 'homepage',
+        component: Homepage
+      },{
+        path:'/priska',
+        name: 'priska',
+        component: Priska
+      },{
+        path:'/propos',
+        name: 'propos',
+        component: Propos
+      },{
+        path:'/projet',
+        name: 'projet',
+        component: Projet
+      },
+    ]
+})
 
 Vue.config.productionTip = false
 
